1d9ab047155d996f44670e351b15615655ead049
vicky kumar001 <vickykumar23499@gmail.com>
2024-11-22 02:18:15 +0530

    Create RotateanArraybyDplace.cpp

---
+++ Array/RotateanArraybyDplace.cpp
@@ -0,0 +1,75 @@
+// C++ Program to left rotate the array by d positions
+// using temporary array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to rotate vector
+void rotateArr(vector<int>& arr, int d) {
+    int n = arr.size();
+
+    // Handle case when d > n
+    d %= n;
+  
+    // Storing rotated version of array
+    vector<int> temp(n);
+
+    // Copy last n - d elements to the front of temp
+    for (int i = 0; i < n - d; i++)
+        temp[i] = arr[d + i];
+
+    // Copy the first d elements to the back of temp
+    for (int i = 0; i < d; i++)
+        temp[n - d + i] = arr[i];
+
+    // Copying the elements of temp in arr
+    // to get the final rotated vector
+    for (int i = 0; i < n; i++)
+        arr[i] = temp[i];
+}
+
+int main() {
+    vector<int> arr = { 1, 2, 3, 4, 5, 6 };
+    int d = 2;
+
+    rotateArr(arr, d);
+
+    // Print the rotated vector
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
+// C++ Code to left rotate an array using Reversal Algorithm
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Function to rotate an array by d elements to the left
+void rotateArr(vector<int>& arr, int d) {
+    int n = arr.size();
+  
+    // Handle the case where d > size of array
+    d %= n;
+  
+    // Reverse the first d elements
+    reverse(arr.begin(), arr.begin() + d);
+
+    // Reverse the remaining n-d elements
+    reverse(arr.begin() + d, arr.end());
+  
+    // Reverse the entire array
+    reverse(arr.begin(), arr.end());
+}
+
+int main() {
+    vector<int> arr = { 1, 2, 3, 4, 5, 6 };
+    int d = 2;
+    
+      rotateArr(arr, d);
+  
+    for (int i = 0; i < arr.size(); i++) 
+        cout << arr[i] << " ";
+    return 0;
+}
